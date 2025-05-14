0a6d30fb6ad2893aed93ac5a8f08dc7550411cb7
Mariam-adel7 <126493222+Mariam-adel7@users.noreply.github.com>
2025-05-14 17:25:33 +0300

    Add files via upload

---
+++ XO GUI/Project1/tictac.cpp
@@ -0,0 +1,15 @@
+﻿#include "tictac.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main()
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Correct namespace and class usage
+    tictac::MyForm^ form = gcnew tictac::MyForm();
+    Application::Run(form);
+}
